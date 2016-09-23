666dd16294ae0f974087c74d9f8cf5cef5236fef
Ananya Thongthai <ananyathongthai@Ananyas-MacBook-Air-4.local>
2016-09-23 15:34:23 +0700

    Add file List-coffee

--- COF-FI/app/pages/list-coffee/list-coffee.ts
+++ COF-FI/app/pages/list-coffee/list-coffee.ts
@@ -11,9 +11,22 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'build/pages/list-coffee/list-coffee.html',
 })
 export class ListCoffeePage {
-
+ items:any = [
+    {name: "iced coffee", type: 1},
+    {name: "black coffee", type: 1},
+    {name: "cappuccino", type: 3},
+    {name: "macchiato", type: 2},
+    {name: "americano", type: 1},
+  ]
   constructor(private navCtrl: NavController) {
 
+  }
+  addNewCoffee(newCoffeName){
+    let newCoffeeObject = {name : newCoffeName};
+    this.items.push(newCoffeeObject);
+  }
+  popUp(selectedPokemon){
+    alert(selectedPokemon.name + "เราเลือกคุณ");
   }
 
 }
